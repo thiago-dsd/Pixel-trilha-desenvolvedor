8f66fb8289153f39fb436b27f53b274d33459a5b
thiago-dsd <thiago.dsd@outlook.com>
2023-05-29 21:32:31 -0300

    Alteração em Q10, finalização de Q12

--- trilha-dev-ex2-23.1/10/index.js
+++ trilha-dev-ex2-23.1/10/index.js
@@ -1,6 +1,10 @@
 // Crie sua função aqui
 function secondBiggestNumber (listaDeNumeros){
-    return listaDeNumeros.sort().reverse()[1]
+    if (listaDeNumeros.length < 2){
+        return null;
+    } else {
+        return listaDeNumeros.sort().reverse()[1]
+    }
 }
 
 // Não altere as linhas abaixo
@@ -9,10 +13,6 @@ console.log(secondBiggestNumber([-1, 10, 20, 30, -10]))
 console.log(secondBiggestNumber([100]))
 
 /*
-    1) Meu node está retornando undefined ao invés de null!
-    2) Mesmo quando coloco crio uma variável para armazenar o sort, ele continua
-    retornando undefined.
-
-    1.b) É possível usar índices negativos em arrays do Javascript? Como
+    1.a) É possível usar índices negativos em arrays do Javascript? Como
     é feito python?
 */
